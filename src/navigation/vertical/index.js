b70850d19e1a87a0e6cba82c4873cbc92268f409
Qamar-Ascend <muhammad.qamar@ascend.com.sa>
2022-04-12 11:37:38 +0500

    second commit

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -1,16 +1,27 @@
-import { Mail, Home } from 'react-feather'
+import { Mail, Home, Users } from "react-feather";
 
 export default [
   {
-    id: 'home',
-    title: 'Home',
-    icon: <Home size={20} />,
-    navLink: '/home'
+    id: "user",
+    title: "Users",
+    icon: <Users size={20} />,
+    children: [
+      {
+        id: "add",
+        title: "Add",
+        navLink: "user/add",
+      },
+      {
+        id: "list",
+        title: "List",
+        navLink: "user/list",
+      },
+    ],
   },
   {
-    id: 'secondPage',
-    title: 'Second Page',
+    id: "secondPage",
+    title: "Second Page",
     icon: <Mail size={20} />,
-    navLink: '/second-page'
-  }
-]
+    navLink: "/second-page",
+  },
+];
